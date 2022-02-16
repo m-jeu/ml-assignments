eda1c28225bba361027ee02515860d9b2a37045d
M <maartenjeu@gmail.com>
2022-02-16 01:25:26 +0100

    Add network

---
+++ ml/src/network.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class PerceptronNetwork:
+
+    def __init__(self, layers: np.ndarray) -> None:
+        self._layers = layers
+
+    def feed_forward(self, input: np.ndarray) -> np.ndarray:
+
+        for layer in self._layers:  # FIXME(m-jeu): Get rid of for loop?
+            input = layer.activate(input)
+
+        return input
