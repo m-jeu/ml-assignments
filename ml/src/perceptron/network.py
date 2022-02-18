e6d685e6c119f615a2cc4e96793c056dd6dbda44
M <maartenjeu@gmail.com>
2022-02-18 21:41:28 +0100

    Add docstrings to PerceptronNetwork

--- ml/src/perceptron/network.py
+++ ml/src/perceptron/network.py
@@ -1,19 +1,42 @@
+"""Module that implements a network of perceptrons."""
+
+
 import numpy as np
 
 from ml.src.perceptron import abstract
 
 
 class PerceptronNetwork(abstract.InOutPutNetworkI):
+    """A network consisting of one or more layers of perceptrons.
+
+    Attributes:
+        _layers: all layers in the network, ordered from first layer to output layer."""
 
     def __init__(self, layers: np.ndarray) -> None:
+        """Initialize instance with _layers."""
         self._layers = layers
 
-    def feed_forward(self, input: np.ndarray) -> np.ndarray:
+    def feed_forward(self, inputs: np.ndarray) -> np.ndarray:
+        """Apply an input to the network.
 
-        for layer in self._layers:  # FIXME(m-jeu): Get rid of for loop?
-            input = layer.feed_forward(input)
+        Applies the input to the first layer, and then passes that layer's output to successive
+        layers until end is reached.
+        Returns input if there are no layers in the network.
 
-        return input
+        Args:
+            inputs: input to apply to the first layer.
+
+        Returns:
+            output of the last layer."""
+
+        for layer in self._layers:  # FIXME(m-jeu): Replace with fancy fold-like operation?
+            inputs = layer.feed_forward(inputs)
+
+        return inputs
 
     def expected_number_of_inputs(self) -> int:
+        """Determine the number of inputs that the network expects.
+
+        Returns:
+            the number of inputs that should be passed to .feed_forward()."""
         return self._layers[0].expected_number_of_inputs()
