988fd979102ae6a7dbcc75dab7bcd162724db374
M <maartenjeu@gmail.com>
2022-02-18 20:59:18 +0100

    Make attributes private

--- ml/src/perceptron/perceptron.py
+++ ml/src/perceptron/perceptron.py
@@ -11,22 +11,21 @@ class Perceptron(abstract.InOutPutNetworkI):
     def __init__(self,
                  weights: np.ndarray,
                  bias: float):
-        self.weights: np.ndarray = weights
-        self.bias: float = bias
-        # FIXME(m-jeu): Refactor attributes to private.
+        self._weights: np.ndarray = weights
+        self._bias: float = bias
 
     def feed_forward(self, inputs: np.ndarray) -> int:
-        return int(np.dot(self.weights, inputs) + self.bias >= 0)  # According to equation in figure 2.3 in reader.
+        return int(np.dot(self._weights, inputs) + self._bias >= 0)  # According to equation in figure 2.3 in reader.
         # Neuron activation function proposed in figure 2.4 is computationally expensive because of O(n) copy operation?
         # That is performed when attempting to insert element at index 0 in numpy array.
         # If change to equation in 2.4 is desired, first element in weights should be 'bias weight'
         # ith 1 added as the first element of every inputs array.
 
     def __str__(self) -> str:
-        return f"( b: {self.bias} )"
+        return f"( b: {self._bias} )"
 
     def __repr__(self) -> str:
         return self.__str__()
 
     def expected_number_of_inputs(self) -> int:
-        return self.weights.size
+        return self._weights.size
